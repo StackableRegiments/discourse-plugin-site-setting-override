2c0443818efeae28a81468e01ebd270ce96c6063
Chris Hagan <chris@stackableregiments.com>
2018-03-12 18:49:55 +1100

    Modifying site_settings at startup

--- plugin.rb
+++ plugin.rb
@@ -1,10 +1,20 @@
 # name: discourse-plugin-unwise-authentication
-# about: Authentication enabling self signup without email and administrator replacement password specification
+# about: Authentication enabling self signup without email and administrator replacement password specification.  Sets password restrictions to dangerously lax.
 # version: 0.0.1
 # authors: Stackable Regiments
 
-add_admin_route 'password_management.title', 'password-management'
+enabled_site_setting :unwise_authentication
 
-Discourse::Admin.routes.append do
-  get '/admin/plugins/password-management' => 'admin/plugins#index', constraints: StaffConstraint.new
+def setMin(field,min)
+  f = SiteSetting.find_by(name: field)
+  f.min = min
+  f.value = min
+  f.save!
+end
+
+
+after_initialize do
+  setMin('min_password_length',1)
+  setMin('min_admin_password_length',1)
+  SiteSetting.block_common_passwords = false
 end
