ff560f8c8ce349451909f388803df93d4befeb75
Chris Hagan <chris@stackableregiments.com>
2018-03-26 23:05:46 +1100

    Added endpoint

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,46 @@
 # name: discourse-plugin-unwise-authentication
-# about: Sets password restrictions to dangerously lax.
-# version: 0.0.1
+# about: Sets password restrictions to dangerously lax and opens an endpoint whereby a user can acquire a new, logged in identity.
+# version: 0.0.2
 # authors: Stackable Regiments
 
 enabled_site_setting :unwise_authentication_enabled
+
+after_initialize do
+  SessionController.class_eval do
+
+    skip_before_action :check_xhr
+
+    def temporary_user
+      username = UserNameSuggester.suggest(I18n.t(:anonymous).downcase)
+      user = User.create!(
+        password: SecureRandom.hex,
+        email: "#{SecureRandom.hex}@anon.#{Discourse.current_hostname}",
+        skip_email_validation:true,
+        name:username,
+        username:username,
+        active:true,
+        trust_level:1,
+        manual_locked_trust_level:1,
+        approved:true,
+        approved_at:1.day.ago,
+        created_at:1.day.ago
+      )
+      user.user_option.update_columns(
+        email_private_messages:false,
+        email_digests:false
+      )
+      user.email_tokens.update_all(confirmed: true)
+      user.activate
+
+      user.reload
+      log_on_user(user)
+
+      redirect_to "/"
+    end
+  end
+
+  Discourse::Application.routes.append do
+    get '/temporary_user' => 'session#temporary_user'
+  end
+
+end
